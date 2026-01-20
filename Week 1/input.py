e9b3a5f55125411b34c33c66756c8d38b5dfe7ab
eunsikim <kimeunsik229@gmail.com>
2026-01-20 16:44:36 -0500

    in class january 20th 2026

---
+++ Week 1/input.py
@@ -0,0 +1,10 @@
+def main():
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+
+    full_name = f"{first_name} {last_name}"
+
+    print(f"Your name is {full_name}")
+
+if __name__ == "__main__":
+    main()
---
+++ Week 1/variable.py
@@ -0,0 +1,14 @@
+def main():
+    first_name = "Eun Sik"
+    last_name = "Kim"
+
+    fullName = first_name + " " + last_name
+
+    # Concatenation
+    print("Your name is " + fullName)
+
+    # Formatted Strings (fStrings)
+    print(f"Your name is {fullName}")
+
+if __name__ == "__main__":
+    main()
