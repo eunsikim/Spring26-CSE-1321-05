0aab71da8df9eaea68a673aad3afbf3d3c3de8cb
eunsikim <kimeunsik229@gmail.com>
2026-03-05 16:30:15 -0500

    in class ex 2 03/05

--- Week 8/in_class_2.py
+++ Week 8/in_class_2.py
@@ -3,14 +3,14 @@
 # Velocity = Distance / Time and Time = Distance / Velocity
 # Create three functions that takes in 2 parameters and return the 
 # result of each of the functions mentioned above.
-def get_distance():
-    pass
+def get_distance(v, t):
+    return v * t
 
-def get_velocity():
-    pass
+def get_velocity(d, t):
+    return d / t
 
-def get_time():
-    pass
+def get_time(d, v):
+    return d / v
 
 # Create a function that takes in three parameters which will represent
 # Distance, Velocity, and Time. These parameters should be optional, with
@@ -22,5 +22,18 @@ def get_time():
 # calculated value.
 # For example: `"Distance: 40"`
 
-def calculate_vdt():
-    pass
+def calculate_vdt(velocity=None, distance=None, time=None):
+    res = ""
+
+    if velocity == None:
+        res += f"Velocity: {get_velocity(distance, time)}"
+    elif distance == None:
+        res += f"Distance: {get_distance(velocity, time)}"
+    else:
+        res += f"Time: {get_time(distance, velocity)}"
+
+    return res
+
+print(calculate_vdt(distance=5, time=6)) # Velocity
+print(calculate_vdt(velocity=5, time=6)) # Distance
+print(calculate_vdt(velocity=5, distance=6)) # Time
