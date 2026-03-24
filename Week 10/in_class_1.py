ad52f2bb7c301a388b43768e849e2b89de6427a1
eunsikim <kimeunsik229@gmail.com>
2026-03-24 16:45:23 -0400

    in class 3/24

--- Week 10/in_class_1.py
+++ Week 10/in_class_1.py
@@ -1,11 +1,21 @@
 # Create a function that takes in a list of floats.
 # The function should calculate the average of the 
 # numbers in the list and return the average.
-def average():
-    pass
+def average(grades_list):
+    grade_sum = 0
+
+    for grade in grades_list:
+        grade_sum += grade
+    
+    grade_average = grade_sum / len(grades_list)
+
+    return grade_average
 
 def main():
-    pass
+    quizzes = [80, 90, 30, 55, 100]
+    quiz_avg = average(quizzes)
+
+    print(f"The average Quiz grade is: {quiz_avg}")
 
 if __name__ == "__main__":
     main()
